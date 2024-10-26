398e82a9ddce7007a0bc0ea4f0a98a8380b94dc9
icaro-v <icaro.vpaula@gmail.com>
2024-10-26 12:39:14 -0300

    remove: remover arquivos desnecessários do repositório

--- pokemon.py
+++
@@ -1,9 +0,0 @@
-from entity import Entity
-
-class Pokemon (Entity):
-    def __init__(self, id, name, type1, type2, description, picture):
-        super().__init__(id, name)
-        self.type1 = type1
-        self.type2 = type2
-        self.description = description
-        self.picture = picture
--- type.py
+++
@@ -1,9 +0,0 @@
-from entity import Entity
-
-class Type(Entity):
-    def __init__(self, id, name, color, strength, weakness):
-        super().__init__(id, name)
-        self.color = color
-        self.strength = strength
-        self.weakness = weakness
-        
